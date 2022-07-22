9eefbcf6605301056358b77bfc8eb6cc42607ee1
Serhiy Yovbak <yovbak.s@gmail.com>
2022-07-22 10:52:55 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,41 @@
+import { useState } from 'react';
 import Section from 'components/Section/Section';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Notification from 'components/Notifications';
 import Statistics from 'components/Statistics';
 import React from 'react';
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  handleInkremet = event => {
-		const { name } = event.currentTarget;
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-		this.setState(prevState => ({
-			[name]: prevState[name] + 1,
-		}));
-		
-	};
-  handleGood = () => {
-    this.setState(prevState => ({ good: prevState.good + 1 }));
-    console.log('Hello Good');
-  };
+  const handleInkremet = event => {
+    const { name } = event.currentTarget;
 
-  handleBad = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-    console.log('Hello Bad');
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
   };
-  handleNeutral = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-    console.log('Hello Neutral');
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
-
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((total, item) => total + item, 0);
-      
+  const countPositiveFeedbackPercentage = () => {
+    return parseInt((good / countTotalFeedback()) * 100);
   };
-
-    countPositiveFeedbackPercentage = () => {
-      const { good } = this.state;
-      let positiveFeedbackPercentage = (good / this.countTotalFeedback()) * 100;
-      if (!positiveFeedbackPercentage) {
-        positiveFeedbackPercentage = 0;
-      }
-      return Math.floor(positiveFeedbackPercentage);
-    };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback;
-    console.log(total);
-    return (
-      <div
+  const total = countTotalFeedback;
+  return (
+    <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -60,41 +44,28 @@ export class App extends React.Component {
         alignItems: 'center',
 
         color: '#010101',
-      }}>
-        <Section title="Please live feedback">
-          <FeedbackOptions
-						options={['good', 'neutral', 'bad']}
-						onLeaveFeedback={this.handleInkremet}
-					/>
-        {/* <FeedbackOptions
-          clickBad={this.handleBad}
-          clickGood={this.handleGood}
-          clickNeutral={this.handleNeutral}
-        /> */}
-       </Section>
-        
-        <Section title="Statistics">
-          {total() !== 0 ? (
-          <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positiveFeedback={this.countPositiveFeedbackPercentage()}
+      }}
+    >
+      <Section title="Please live feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleInkremet}
         />
+      </Section>
+
+      <Section title="Statistics">
+        {total() !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positiveFeedback={countPositiveFeedbackPercentage()}
+          />
         ) : (
-           <Notification message="There is no feedback" />
+          <Notification message="There is no feedback" />
         )}
-        </Section>
-        
-        
-        
-      </div>
-    );
-  }
-}
-
-
-
-
-
+      </Section>
+    </div>
+  );
+};
